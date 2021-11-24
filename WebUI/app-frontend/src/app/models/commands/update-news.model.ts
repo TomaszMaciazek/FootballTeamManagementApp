aff8bc7b92e0be43fc7e006bf5d1bcc68e4b836f
TomaszMaciazek <tommac.opole@gmail.com>
2021-11-24 22:02:37 +0100

    Added update news commnad

---
+++ WebUI/app-frontend/src/app/models/commands/update-news.model.ts
@@ -0,0 +1,15 @@
+export class UpdateNews {
+    public id: string;
+    public title: string;
+    public content: string;
+    public isImportant: boolean;
+
+    constructor(data: {
+        id: string,
+        title: string,
+        content: string,
+        isImportant: boolean
+    }){
+        Object.assign(this, data);
+    }
+}
